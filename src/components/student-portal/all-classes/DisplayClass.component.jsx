6197fd200ebae119698c769ee6f263676366603e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-02 15:42:52 -0600

    Added descriptions to the data as well as change the formatting of some of the output

--- src/components/student-portal/all-classes/DisplayClass.component.jsx
+++ src/components/student-portal/all-classes/DisplayClass.component.jsx
@@ -1,10 +1,13 @@
 export default function DisplayClass({ classDetails}) {
     return (
-        <div className="flex flex-col w-full shadow-lg border-black items-center bg-white rounded-lg">
+        <div className="flex flex-col w-full shadow-lg border-black items-center p-1 bg-white rounded-lg">
             
             <h1 className="text-2xl font-bold mb-2">{classDetails.name}</h1>
-            {/* <p className="mb-2">{classDetails.description}</p> */}
+            <p className="mb-2">{classDetails.description}</p>
             <p>Available fall: {classDetails.availableFall ? 'Yes' : 'No'}</p>
+            <p>Available winter: {classDetails.availableWinter ? 'Yes' : 'No'}</p>
+            <p>Available spring: {classDetails.availableSpring ? 'Yes' : 'No'}</p>
+            <p>Available summer: {classDetails.availableSummer ? 'Yes' : 'No'}</p>
         </div>
     )
 }
