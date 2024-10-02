b37de61443630cc02056fb41dbff52d041b26249
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-02 16:07:34 -0600

    added component

---
+++ src/components/bvc-image/BvcWhiteImage.component.jsx
@@ -0,0 +1,7 @@
+export default function BVCImage({height}) {
+    return (
+        <>
+            <img src={"/BvcWhite.png"} alt={"BVC"} style={{ height: `${height}px` }} />
+        </>
+    )
+}
