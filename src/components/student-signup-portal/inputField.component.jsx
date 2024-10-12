b239cfacae956d5948862564e42c85bd0e1ac271
Thiago Lima <thiago_lima19@hotmail.com>
2024-10-11 20:47:08 -0600

    improving the input field component

--- src/components/student-signup-portal/inputField.component.jsx
+++ src/components/student-signup-portal/inputField.component.jsx
@@ -1,54 +1,45 @@
 import React from "react";
 import { TextField } from "@mui/material";
 
-const InputField = ({
-  id,
-  name,
-  label,
-  type = "text",
-  value,
-  onChange,
-  required = true,
-  readOnly = false,
-}) => {
-  return (
-    <TextField
-      id={id}
-      name={name}
-      label={label}
-      type={type}
-      value={value}
-      onChange={onChange}
-      required={required}
-      InputProps={{
-        readOnly: readOnly,
-      }}
-      fullWidth
-      variant="standard"
-      sx={{
-        "& .MuiInputLabel-root": {
-          color: "white",
-          "&.Mui-focused": {
-            color: "white",
-          },
-        },
-        "& .MuiInput-underline": {
-          "&:before": {
-            borderBottomColor: "white",
-          },
-          "&:after": {
-            borderBottomColor: "white",
-          },
-        },
-        "& .MuiInputBase-input": {
-          color: "white",
-          "&:focus": {
-            color: "white",
-          },
-        },
-      }}
-    />
-  );
-};
-
-export default InputField;
+const InputField = ({ id, name, label, value, onChange, type = "text", ...rest }) => {
+    return (
+        <TextField
+          id={id}
+          name={name}
+          label={label}
+          variant="standard"
+          value={value}
+          onChange={onChange}
+          type={type}
+          fullWidth
+          required{...rest}
+          sx={{
+            '& .MuiInputLabel-root': {
+                color: 'white',
+                '&.Mui-focused': {
+                    color: 'white'
+                }
+            },
+            '& .MuiInput-underline': {
+                '&:before': {
+                    borderBottomColor: 'white'
+                },
+                '&:after': {
+                    borderBottomColor: 'white'
+                }
+            },
+            '& .MuiInputBase-input': {
+                '&:focus': {
+                    color: 'white'
+                },
+                '&:not(:focus)': {
+                    color: 'white'
+                }
+            }
+        }}
+          
+        />
+      );
+    };
+    
+    export default InputField;
