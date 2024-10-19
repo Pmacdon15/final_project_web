87b9366a17bc9688e5cb67a2b1d82f952b8bcd03
mariannaAntunes <marianna_antunes@outlook.com>
2024-10-19 14:31:49 -0300

    uptated dashboard with personal infos

---
+++ src/components/student-portal/dashboard/DisplayStudentData.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { LoadAllPrograms } from "../../../placeholders/load-data/loadData.action"
+
+
+export default function DisplayStudentData({student, classes}) {
+    const [allPrograms, setAllPrograms] = React.useState([]);
+
+    React.useEffect(() => {
+    const fetchAllPrograms = async () => {
+        const loadedAllPrograms = LoadAllPrograms();
+        setAllPrograms(loadedAllPrograms);
+    };
+    fetchAllPrograms();
+    }, []);
+    
+    return (
+        <div className = "flex flex-col w-full shadow-lg border-black items-center bg-white rounded-lg">
+            <h1 className = "text-2xl font-bold mb-2">{student.name}</h1>
+            <p ClassName="mb-2">ID: {student.id}</p>
+            <p ClassName="mb-2">Email: {student.email}</p>
+            <p ClassName="mb-2">Phone Number: {student.phoneNum}</p>
+            <p ClassName="mb-2">Address: {student.address}</p>
+            
+            {/* TODO: Add onclick event to update student personal information*/}
+            <button className="flex flex-col m-4 px-4 py-2 w-1.5/6 items-center bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
+                Update Personal Information
+            </button>
+            <h2 className="text-xl font-bold mb-2">Program: </h2>
+            <h2 className="text-xl font-bold mb-2">Classes: </h2>
+            {classes.map((classItem,index) => (
+                <h3 key={index} className="mb-2"> {classItem.name}</h3>
+            ))}
+        </div>
+    )
+
+}
--- src/components/student-portal/dashboard/dashboard.page.jsx
+++ src/components/student-portal/dashboard/dashboard.page.jsx
@@ -1,5 +1,36 @@
+import DisplayStudentData from './DisplayStudentData.component.jsx';
+import {LoadUserClasses} from '../../../placeholders/load-data/loadData.action.js';
+import {LoadUserData} from '../../../placeholders/load-data/loadData.action.js';
 import React from 'react';
+import getUserInfo from '../../../utils/get-user-info';
 
 export default function StudentPortalDashBoard() {
-    return <>Dashboard</>;
+    const { email } = getUserInfo();
+    console.log("USer email:", email);
+
+    const [userData, setUserData] = React.useState([]);
+    const [userClasses, setUserClasses] = React.useState([]);
+
+    React.useEffect(() => {
+        const fetchUserData = async () => {
+            const loadedUsersData = LoadUserData();
+            const loadedUserData = loadedUsersData.find (student => student.email === email);
+            setUserData(loadedUserData);
+        };
+        fetchUserData();
+        const fetchUserClasses = async () => {
+            const loadedUserClasses = LoadUserClasses();
+            setUserClasses(loadedUserClasses);
+        };
+        fetchUserClasses();
+    }, [email]);
+
+    return (
+        <div className="flex flex-col w-full gap-4 justify-center items-center ">
+            <div className=' bg-blue-100  shadow-lg gap-4   p-4 md:p-5 border rounded-lg '>
+                <h1 className='text-2xl font-bold mb-2'>My Dashboard</h1>
+            </div>
+            <DisplayStudentData student={userData} classes={userClasses}/>
+        </div>
+    );
 }
