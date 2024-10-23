6e0b7dc03482de9430c738a95cb8412d3bc7ca87
Thiago Lima <thiago_lima19@hotmail.com>
2024-10-23 16:29:59 -0600

    formating the displayProgramComponent, and add the creat, edit and delete buttons

--- src/components/student-portal/all-programs/DisplayProgram.component.jsx
+++ src/components/student-portal/all-programs/DisplayProgram.component.jsx
@@ -1,33 +1,53 @@
-export default function DisplayProgram({ program, isAdmin, onEdit, onDelete}) {
-  
-    return (
-        <div
-            className={
-                "flex flex-col w-full shadow-lg border-black items-center bg-white rounded-lg hover:shadow-2xl hover:border-blue-500 transition duration-300"
-            }
-        >
-            <h1 className="text-2xl font-bold mb-2">{program.name}</h1>
-            <p className="mb-2">{program.description}</p>
-            <p className="mb-2">Amount of terms: {program.durationTerms}</p>
-            <p>Tuition: ${program.tuition}</p>
-            {isAdmin && (
-                                <div className="flex gap-4 mt-2">
-                                    <button
-                                        className="bg-green-600 p-2 text-white font-extrabold rounded-lg shadow-lg hover:scale-110 hover:bg-green-700"
-                                        onClick={() => onEdit(program)}
-                                    >
-                                        Edit
-                                    </button>
+import { DeleteProgramFromLocalStorage } from "../../../placeholders/load-data/loadData.action";
 
-                                    <button
-                                        className="bg-red-600 p-2 text-white font-extrabold rounded-lg shadow-lg hover:scale-110 hover:bg-red-700"
-                                        onClick={() => onDelete(program.id)}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            )}
+export default function DisplayProgram({ program, isAdmin, onEdit, onDelete }) {
+  return (
+    <div
+      className={
+        "flex flex-col w-full shadow-lg border-black items-center bg-white rounded-lg hover:shadow-2xl hover:border-blue-500 transition duration-300"
+      }
+    >
+      <h1 className="text-2xl font-bold mb-2">{program.name}</h1>
+      {isAdmin && (
+        <p className="text-xl font-bold mb-2">
+          Program ID:{" "}
+          <span className="text-xl font-normal mb-2"> {program.id}</span>{" "}
+        </p>
+      )}
+      <div>
+        <p className="text-xl font-bold mb-2">Program Description:</p>
+        <p className="mb-2">{program.description}</p>
+      </div>
+      <p className="text-xl font-bold mb-2">
+        Amount of terms:{" "}
+        <span className="text-xl font-normal mb-2">
+          {program.durationTerms}
+        </span>{" "}
+      </p>
+      <p className="text-xl font-bold mb-2">
+        Tuition:{" "}
+        <span className="text-xl font-normal mb-2"> ${program.tuition}</span>{" "}
+      </p>
+      {isAdmin && (
+        <div className="flex gap-4 my-2">
+          <button
+            className="bg-green-600 p-2 text-white font-extrabold rounded-lg shadow-lg hover:scale-110 hover:bg-green-700"
+            onClick={() => onEdit(program)}
+          >
+            Edit
+          </button>
 
+          <button
+            className="bg-red-600 p-2 text-white font-extrabold rounded-lg shadow-lg hover:scale-110 hover:bg-red-700"
+            onClick={() => {
+              DeleteProgramFromLocalStorage(program.id);
+              onDelete(program.id);
+            }}
+          >
+            Delete
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 }
