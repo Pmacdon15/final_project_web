81d2a9e118f9f00ec13d322f50cb7676bc8ce0e0
Thiago Lima <thiago_lima19@hotmail.com>
2024-10-24 09:23:56 -0600

    make some changes on the login action to retreive data from local storage

--- src/placeholders/authentication/login.action.js
+++ src/placeholders/authentication/login.action.js
@@ -1,37 +1,71 @@
 import users from './user.data.json';
 
+// function handleLogin(username, password) {
+//     if (!username || !password) {
+//         throw new Error('Username and Password should not be empty !');
+//     }
+
+//     const userExists = users.find(function (user) {
+//         if (user.email === username && user.password === password) {
+//             return true;
+//         }
+
+//         return false;
+//     });
+
+//     if (!userExists) {
+//         return false;
+//     }
+
+//     //set user information to session storage
+//     sessionStorage.setItem(
+//         'BVC_Session',
+//         JSON.stringify({
+//             name: userExists.name,
+//             isAdmin: userExists.isAdmin,
+//             id: userExists.id,
+//             email: userExists.email
+//         })
+//     );
+
+//     return {
+//         name: userExists.name,
+//         isAdmin: userExists.isAdmin
+//     };
+// }
 function handleLogin(username, password) {
     if (!username || !password) {
-        throw new Error('Username and Password should not be empty !');
+        throw new Error('Username and Password should not be empty!');
     }
 
-    const userExists = users.find(function (user) {
-        if (user.email === username && user.password === password) {
-            return true;
-        }
+    // Get the stored user data from local storage
+    const users = JSON.parse(localStorage.getItem('userData')) || [];
 
-        return false;
+    // Check if the user exists
+    const userExists = users.find(function (user) {
+        return user.email === username && user.password === password;
     });
 
     if (!userExists) {
-        return false;
+        return false; // Invalid credentials
     }
 
-    //set user information to session storage
+    // Set the user information to session storage
     sessionStorage.setItem(
         'BVC_Session',
         JSON.stringify({
             name: userExists.name,
             isAdmin: userExists.isAdmin,
             id: userExists.id,
-            email: userExists.email
+            email: userExists.email,
         })
     );
 
     return {
         name: userExists.name,
-        isAdmin: userExists.isAdmin
+        isAdmin: userExists.isAdmin,
     };
 }
+
 
 export default handleLogin;
