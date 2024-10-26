b72883d956a13a1975ea820df15b0132b591afd2
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-25 19:11:31 -0600

    styled

--- src/components/admin-portal/view_students/ViewStudents.page.jsx
+++ src/components/admin-portal/view_students/ViewStudents.page.jsx
@@ -13,14 +13,17 @@ export default function AdminPortalViewStudents() {
     }, []);
 
     return (
-        <div >
-            <div>
+        <div className='flex flex-col p-2 md:p-4 gap-4 w-full justify-center items-center ' >
+            <div className='bg-blue-100 p-4 rounded-lg w-4/6 md:w-1/6'>
                 <h1>Students</h1>
                 <p>Here you will be able to View all students.</p>
             </div>
-            <div>
+            <div className='flex flex-wrap bg-blue-100 justify-center rounded-lg w-full md:w-5/6 gap-4 p-2 md:gap-1 shadow-lg'>
                 {students.map((student) => (
-                    <div key={student.id}>
+                    <div
+                        key={student.id}
+                        className='flex flex-col w-[40%] md:w-1/6 bg-white p-4 rounded-lg shadow-lg   md:m-2 gap-2'
+                    >
                         <p>First Name: {student.firstName}</p>
                         <p>Last Name: {student.lastName}</p>
                         <p>Student ID: {student.id}</p>
