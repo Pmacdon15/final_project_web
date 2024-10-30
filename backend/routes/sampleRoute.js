814d30b45df8aa2d4c3b511fb2f998efb53ae046
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-10-30 16:13:14 -0600

    added sample files and db config file

---
+++ backend/routes/sampleRoute.js
@@ -0,0 +1,26 @@
+
+// ! This is a sample file so the directory would upload with my commit  and  so we can see what a route should look like
+
+
+/*
+Routes for Advanced APIs
+This file defines the route for the advanced operations.
+*/
+
+// import express from 'express';
+// import {
+//   getUsersWithOrders,
+//   getUsersWithOrdersAndProducts,
+//   getTotalAmountSpentByUsers,
+//   getMostOrderedProductPerUser,
+// } from '../controllers/orderController.js'; // Import the functions
+
+// const router = express.Router();
+
+// router.get('/users-with-orders', getUsersWithOrders); 
+// router.get('/users-with-orders-products', getUsersWithOrdersAndProducts);
+// router.get('/total-amount-spent', getTotalAmountSpentByUsers);
+// router.get('/most-ordered-product', getMostOrderedProductPerUser);
+
+// export default router;
+
