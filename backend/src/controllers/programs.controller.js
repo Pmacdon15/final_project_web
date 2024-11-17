be238b5ddc159f822abcb31ec823ad3ef0cf5994
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-11-16 19:57:47 -0700

    Add removeProgram functionality and update getAllPrograms response status

--- backend/src/controllers/programs.controller.js
+++ backend/src/controllers/programs.controller.js
@@ -2,15 +2,29 @@ import sql from 'mssql';
 import { config } from '../db/index.js'
 
 export const getAllPrograms = async (req, res) => {
-    try {
-      console.log('getAllPrograms called');
-      await sql.connect(config);
-      const result = await sql.query('SELECT * FROM programs');
-      console.log('Query result:', result.recordset);
-      res.json(result.recordset);
-    } catch (err) {
-      console.error('Error fetching programs data:', err);
-      res.status(500).json({ error: 'Failed to retrieve programs data' });
-    }
-  };
+  try {
+    console.log('getAllPrograms called');
+    await sql.connect(config);
+    const result = await sql.query('SELECT * FROM programs');
+    console.log('Query result:', result.recordset);
+    res.status(200).json(result.recordset);
+  } catch (err) {
+    console.error('Error fetching programs data:', err);
+    res.status(500).json({ error: 'Failed to retrieve programs data' });
+  }
+};
+
+//MARK: Remove program
+export const removeProgram = async (req, res) => {
+  try {
+    console.log('removeProgram called');
+    await sql.connect(config);
+    const result = await sql.query`DELETE FROM programs WHERE id = ${req.params.id}`;
+    console.log('Query result:', result);
+    res.status(200).json({ message: 'Program deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting program:', err);
+    res.status(500).json({ error: 'Failed to delete program' });
+  }
+};
 
