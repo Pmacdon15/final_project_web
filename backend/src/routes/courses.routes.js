3ac249e260a379d25f3c0fd4033210ac4401ee02
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-11-16 18:44:15 -0700

    Add removeCourse functionality and update foreign key constraints for cascading deletes

--- backend/src/routes/courses.routes.js
+++ backend/src/routes/courses.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getAllCourses, addUserCourse, removeUserCourse } from '../controllers/courses.controller.js';
+import { getAllCourses, addUserCourse, removeUserCourse, removeCourse } from '../controllers/courses.controller.js';
 const router = express.Router();
 
 router.get('/get-all-courses', getAllCourses);
 router.post('/add-user-course/userId/:userId/courseId/:courseId/userTermId/:userTermId/termSeason/:termSeason', addUserCourse);
-router.get('/remove-user-course/userId/:userId/courseId/:courseId', removeUserCourse);
+router.delete('/remove-user-course/userId/:userId/courseId/:courseId', removeUserCourse);
+
+router.delete('/remove-course/courseId/:courseId', removeCourse);
+
+
 
 export default router;
 
