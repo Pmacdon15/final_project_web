7df38f0157099c84156da2d6c09b650d6bc02cc7
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-11-16 21:46:04 -0700

    add route to create a new program and implement addProgram controller

--- backend/src/controllers/programs.controller.js
+++ backend/src/controllers/programs.controller.js
@@ -2,15 +2,57 @@ import sql from 'mssql';
 import { config } from '../db/index.js'
 
 export const getAllPrograms = async (req, res) => {
-    try {
-      console.log('getAllPrograms called');
-      await sql.connect(config);
-      const result = await sql.query('SELECT * FROM programs');
-      console.log('Query result:', result.recordset);
-      res.json(result.recordset);
-    } catch (err) {
-      console.error('Error fetching programs data:', err);
-      res.status(500).json({ error: 'Failed to retrieve programs data' });
-    }
-  };
+  try {
+    console.log('getAllPrograms called');
+    await sql.connect(config);
+    const result = await sql.query('SELECT * FROM programs');
+    console.log('Query result:', result.recordset);
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error fetching programs data:', err);
+    res.status(500).json({ error: 'Failed to retrieve programs data' });
+  }
+};
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MARK: Add a new program
+export const addProgram = async (req, res) => {
+  try {
+    console.log('addProgram called');
+    await sql.connect(config);
+    
+    const { name, description, durationTerms, tuition } = req.body;
+    
+    if (!name || !description || !durationTerms || !tuition) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const result = await sql.query`
+        INSERT INTO programs (name, description, durationTerms, tuition)
+        VALUES (${name}, ${description}, ${durationTerms}, ${tuition});        
+      `;
+    if (result.rowsAffected[0] !== 1) {
+      throw new Error('Failed to insert program');
+    }
+    console.log('Query result:', result);
+    res.status(201).json({ message: 'Program added successfully' });
+
+  } catch (err) {
+    console.error('Error adding program:', err);
+    res.status(500).json({ error: 'Failed to add program' });
+  }
+}
--- backend/src/routes/programs.routes.js
+++ backend/src/routes/programs.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getAllPrograms } from '../controllers/programs.controller.js';
+import { getAllPrograms, addProgram } from '../controllers/programs.controller.js';
 const router = express.Router();
 
 router.get('/get-all-programs', getAllPrograms);
+
+
+router.post('/add-program', addProgram);
 
 export default router;
 
