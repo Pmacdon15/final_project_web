9b9d9b8ced2a4a17bdb8ab558ab838f13f1b7193
Thiago Lima <thiago_lima19@hotmail.com>
2024-11-20 21:12:10 -0700

    creating the sign uo route file

---
+++ backend/src/routes/signUp.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/signUp.controller.js";
+
+const router = express.Router();
+
+// Define routes for users
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+
+export default router;
