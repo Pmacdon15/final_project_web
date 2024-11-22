b43d00c87a61ef28fb84f9bc4d194875abf8d5ea
Thiago Lima <thiago_lima19@hotmail.com>
2024-11-21 21:29:30 -0700

    update the auth middleware with jwt

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,19 +1,3 @@
-// import jwt from 'jsonwebtoken';
-
-// export const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader && authHeader.split(' ')[1];
-  
-//   if (!token) return res.status(401).json({ error: 'Unauthorized access' });
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if (err) return res.status(403).json({ error: 'Forbidden access' });
-//     req.user = user; // Populate req.user with user data from token
-//     next();
-//   });
-// };
-
-// src/middlewares/auth.middleware.js
 import jwt from 'jsonwebtoken';
 
 /**
