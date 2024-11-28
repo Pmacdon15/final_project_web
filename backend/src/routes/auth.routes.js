28e096c6ee7ff5110c4e21e277b0b4edafb739e0
Manu <manuellafflopes@gmail.com>
2024-11-28 14:22:19 -0700

    fixing merge errors

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,11 @@
 // src/routes/auth.routes.js
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import {
+  login,
+  register,
+  logout,
+  changePassword,
+} from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
