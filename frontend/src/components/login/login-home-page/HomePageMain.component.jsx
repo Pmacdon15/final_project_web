45313bcc442287328530325d87caa16afa095136
Thiago Lima <thiago_lima19@hotmail.com>
2024-12-07 11:10:17 -0700

    adpating the home page main to inclue only the button to login and guest

--- frontend/src/components/login/login-home-page/HomePageMain.component.jsx
+++ frontend/src/components/login/login-home-page/HomePageMain.component.jsx
@@ -1,11 +1,29 @@
 // import './App.css';
+// import ButtonLink from "./ButtonLink";
+// export default function HomePageMain() {
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-4 p-4  w-full md:w-2/6 rounded-lg  text-white bg-blue-500">
+//       <h1>Access portal</h1>
+//       {/* <ButtonLink text="Student" link="/student-login" />
+//       <ButtonLink text="Admin" link="/admin-login" /> */}
+//        <a href="/login">
+//         <button className="LoginHomePageButtonLink">Login</button>
+//       </a>
+//       <a href="/guest/all-programs">
+//         <button className="LoginHomePageButtonLink">Guest Access</button>
+//       </a>
+//       <a href="/student-signup">Click here to Sign Up</a>
+//     </div>
+//   );
+// }
+
 import ButtonLink from "./ButtonLink";
+
 export default function HomePageMain() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4  w-full md:w-2/6 rounded-lg  text-white bg-blue-500">
+    <div className="flex flex-col items-center justify-center gap-4 p-4 w-full md:w-2/6 rounded-lg text-white bg-blue-500">
       <h1>Access portal</h1>
-      <ButtonLink text="Student" link="/student-login" />
-      <ButtonLink text="Admin" link="/admin-login" />
+      <ButtonLink text="Login" link="/login" />
       <a href="/guest/all-programs">
         <button className="LoginHomePageButtonLink">Guest Access</button>
       </a>
